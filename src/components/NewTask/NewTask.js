3161979ee89e562d6cc70fc569cec26e478a6228
Abhishek Gupta <abhishekgupt.in@gmail.com>
2021-06-10 04:52:07 +0530

    Completed Todo App

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,38 @@
+import useHttp from "../../hooks/use-http";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: postTask } = useHttp();
+
+  const generateTask = (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    postTask(
+      {
+        url: "https://react-http-241d3-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+        method: "POST",
+        body: { text: taskText },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      generateTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
